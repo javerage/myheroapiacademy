0bceccc761319ffffd322dac882ca06e0a48c591
Javerage <joel.chuc@gmail.com>
2024-10-08 21:23:53 -0600

    install and configurate cors fron all origins

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import cors from 'cors'
+
 import heroController from './controllers/heroController.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', heroController)
 
